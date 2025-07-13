20d8aacdb49a7a80f4cfc8d0489c6f949c1c7393
IngeCesar <cooh1@ymail.com>
2025-07-13 15:33:06 -0600

    Se ahorro codigo con el concepto de poliformismo en las clases de CalculadoraDeTiempo y la clase Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
@@ -25,13 +26,17 @@ public class Principal {
         casaDragon.muestraFichaTecnica();
         System.out.println(casaDragon.getDuracionEnMinutos());
 
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
 
-//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
-//        otraPelicula.nombre = "Matrix";
-//        otraPelicula.fechaDeLanzamiento = 1998;
-//        otraPelicula.duracionEnMinutos = 180;
-//
-//        otraPelicula.muestraFichaTecnica();
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus títulos favoritos estas vacaciones "
+                +calculadora.getTiempoTotal() + " minutos");
 
     }
 }
--- src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
+++ src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -1,11 +1,14 @@
 package com.aluracursos.screenmatch.calculos;
-
-import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTiempo {
     private int tiempoTotal;
 
-    public void incluye(Pelicula pelicula){
-        tiempoTotal += pelicula.getDuracionEnMinutos();
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+
+    public void incluye(Titulo titulo){
+        this.tiempoTotal += titulo.getDuracionEnMinutos();
     }
 }
